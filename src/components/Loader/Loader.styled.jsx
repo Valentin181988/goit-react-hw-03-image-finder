d56114819c61b9060976c49a3cb5be9b75e4db55
Valentin181988 <85246610+Valentin181988@users.noreply.github.com>
2022-01-20 21:13:09 +0300

    Loader

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
 
 export const LoaderWrapper = styled.div`
-   display: block;
-   text-align: center;
-   margin: 0 auto;
-   width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 500px;
+  width: 100%;
+  border: none;
 `;
