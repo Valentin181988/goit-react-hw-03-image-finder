561c1757a3d5291fa6cf2329b903be22a8761444
Valentin181988 <85246610+Valentin181988@users.noreply.github.com>
2022-01-21 20:22:22 +0300

    styles

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,4 +1,4 @@
-import { ImageGalleryItems } from "./ImageGalleryItem.styled";
+import { ImageGalleryItems, ImageGalleryItemImg } from "./ImageGalleryItem.styled";
 import PropTypes from 'prop-types';
 
 export const ImageGalleryItem = ({pictures, onClick}) => {
@@ -7,7 +7,7 @@ export const ImageGalleryItem = ({pictures, onClick}) => {
         {pictures.map(picture => {
           return(
               <ImageGalleryItems key={picture.id} onClick={() => onClick(picture)}>
-                  <img src={picture.webformatURL} alt={picture.tag} />
+                  <ImageGalleryItemImg src={picture.webformatURL} alt={picture.tag} />
               </ImageGalleryItems>
           );
         })}
